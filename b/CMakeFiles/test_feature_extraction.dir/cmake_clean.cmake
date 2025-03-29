47c5791524639e3ec23acec6ceac76db5f1e1c81
Sofiia <popeniuk.pn@ucu.edu.ua>
2025-03-29 19:29:51 +0200

    corrected

---
+++ b/CMakeFiles/test_feature_extraction.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_feature_extraction.dir/FREAK_feature_descriptor.cpp.o"
+  "CMakeFiles/test_feature_extraction.dir/FREAK_feature_descriptor.cpp.o.d"
+  "CMakeFiles/test_feature_extraction.dir/corner_detection.cpp.o"
+  "CMakeFiles/test_feature_extraction.dir/corner_detection.cpp.o.d"
+  "CMakeFiles/test_feature_extraction.dir/matching.cpp.o"
+  "CMakeFiles/test_feature_extraction.dir/matching.cpp.o.d"
+  "CMakeFiles/test_feature_extraction.dir/test_feature_extraction.cpp.o"
+  "CMakeFiles/test_feature_extraction.dir/test_feature_extraction.cpp.o.d"
+  "test_feature_extraction"
+  "test_feature_extraction.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_feature_extraction.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
