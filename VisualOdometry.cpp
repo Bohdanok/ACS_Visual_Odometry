e162ccbd5c036fe5242d9de6362834814c3b8e6c
Bohdanok <bohdan.milian@gmail.com>
2025-05-10 12:37:25 +0300

    Classes for VO feature extraction and feature description fixed

--- VisualOdometry.cpp
+++ VisualOdometry.cpp
@@ -4,21 +4,33 @@
 
 #include "VisualOdometry.h"
 
-VisualOdometry::VisualOdometry(const size_t number_of_threads) : VO_pool(number_of_threads), number_of_threads(number_of_threads) {
-    // + SOFIA SOME CODE FOR VO IDK
+VisualOdometry::VisualOdometry(
+    const std::string& kernel_filename,
+    const std::size_t num_threads)
+  : VO_pool(num_threads)
+  , number_of_threads(num_threads)
+  , gpu_settings([&]() {
+      // load binary and create OpenCL program
+      const auto program = create_platform_from_binary(kernel_filename);
+      const auto devices = program.getInfo<CL_PROGRAM_DEVICES>();
+      if (devices.empty()) {
+          throw std::runtime_error("No OpenCL devices found for the program.");
+      }
+      const auto& device = devices.front();
+      const auto context = program.getInfo<CL_PROGRAM_CONTEXT>();
+      return GPU_settings{program, device, context};
+  }())
+  , kernel_filename(kernel_filename)
+{   }
+
+
+// READ in grayscale already
+std::pair<std::vector<std::vector<uint8_t>>, std::vector<cv::KeyPoint>> VisualOdometry::compute_descriptor_with_key_points(const cv::Mat &image) {
+    return feature_extraction_manager_with_points(image, this->gpu_settings);
 }
 
-std::vector<std::vector<uint8_t>> VisualOdometry::compute_descriptor(cv::Mat &image) {
-    if (image.channels() != 1) image = CornerDetectionParallel::custom_bgr2gray(image);
-    return feature_extraction_manager(image, VO_pool);
-}
-
-std::pair<std::vector<std::vector<uint8_t>>, std::vector<cv::KeyPoint>> VisualOdometry::compute_descriptor_with_key_points(cv::Mat &image) {
-    if (image.channels() != 1) image = CornerDetectionParallel::custom_bgr2gray(image);
-    return feature_extraction_manager_with_points(image, VO_pool);
-}
 
 std::vector<std::pair<int, int>> VisualOdometry::match_descriptors(const std::vector<std::vector<uint8_t>> &desc1, const std::vector<std::vector<uint8_t>> &desc2) {
-    return matchCustomBinaryDescriptorsThreadPool(desc1, desc2, VO_pool, number_of_threads);
+    return matchCustomBinaryDescriptorsThreadPool(desc1, desc2, this->VO_pool, this->number_of_threads, 0.75f);
 }
 
--- VisualOdometry.h
+++ VisualOdometry.h
@@ -1,25 +1,30 @@
-//
-// Created by julfy on 20.04.25.
-//
+#ifndef VISUAL_ODOMETRY_H
+#define VISUAL_ODOMETRY_H
 
-#ifndef MAIN_H
-#define MAIN_H
+#include <cstddef>
+#include <string>
+#include <vector>
+#include <opencv2/core.hpp>
 
-#include "feature_extraction_parallel/feature_extraction_parallel.h"
+#include "feature_extraction_parallel_GPU/feature_extraction_parallel_GPU.h"
 #include "feature_matching_parallel/feature_matching_parallel.h"
-
 
 class VisualOdometry {
 private:
     thread_pool VO_pool;
-    const size_t number_of_threads;
+    const std::size_t number_of_threads;
+    GPU_settings gpu_settings;
+    const std::string kernel_filename;
+
 public:
-    explicit VisualOdometry(const size_t number_of_threads);
-    std::vector<std::vector<uint8_t>> compute_descriptor(cv::Mat& image);
-    std::pair<std::vector<std::vector<uint8_t>>, std::vector<cv::KeyPoint>> compute_descriptor_with_key_points(cv::Mat& image);
-    std::vector<std::pair<int, int>> match_descriptors(const std::vector<std::vector<uint8_t>>& desc1, const std::vector<std::vector<uint8_t>>& desc2);
+    explicit VisualOdometry(const std::string& kernel_filename, std::size_t num_threads);
+    // std::vector<std::vector<uint8_t>> compute_descriptor(cv::Mat& image);
+    std::pair<std::vector<std::vector<uint8_t>>, std::vector<cv::KeyPoint>>
+        compute_descriptor_with_key_points(const cv::Mat& image);
+    std::vector<std::pair<int, int>> match_descriptors(
+        const std::vector<std::vector<uint8_t>>& desc1,
+        const std::vector<std::vector<uint8_t>>& desc2);
+    void run(const std::string& image_directory, std::size_t num_of_runs);
 };
 
-
-
-#endif //MAIN_H
+#endif // VISUAL_ODOMETRY_H
